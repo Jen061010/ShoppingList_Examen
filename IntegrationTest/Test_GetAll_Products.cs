0757e60077961e3682b5e6b4faf02566f40d8c06
Jennifer <jvs1192@gmail.com>
2016-05-06 14:47:30 +0200

    IntegrationTest

---
+++ IntegrationTest/Test_GetAll_Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebService;
+
+namespace IntegrationTest
+{
+    [TestClass]
+    public class IntegrationTest
+
+    {
+        private readonly int Product = 4;
+        private ServiceProduct _serviceProduct;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+           _serviceProduct = new ServiceProduct();
+            using(var ctx=new RepositoryProduct()){
+                if (ctx.Database.Exists())
+                    ctx.Database.Delete();
+                ctx.Database.Create();
+            }
+        }
+
+        [TestMethod]
+        public void Test_GetAll_Product()
+        {
+           
+        }
+       
+            
+        
+    }
+}
